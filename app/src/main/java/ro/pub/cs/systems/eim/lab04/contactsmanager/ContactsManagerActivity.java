53697c77fddf491490965a167aa19e777483a485
victoriatascau <vickytascau@gmail.com>
2021-03-30 14:29:09 +0300

    Initial commmit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.lab04.contactsmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactsManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_manager);
+    }
+}
